802cb85681e5fad67152a1d7634dd73bb48e65a8
Younghoon <gcloud1025@gmail.com>
2022-01-24 21:53:24 +0900

    codeup4

--- practice.py
+++ practice.py
@@ -1,2 +1,14 @@
-w = int(input())
-print(w[0:2], w[2:4], w[4,6])
+def get_strong_word(a, b):
+    result_a = 0
+    result_b = 0
+    for i in a:
+        result_a += ord(i)
+    for i in b:
+        result_b += ord(i)
+    if result_a > result_b:
+        return a
+    else:
+        return b
+        
+print(get_strong_word('z', 'a'))
+print(get_strong_word('tom', 'john'))
